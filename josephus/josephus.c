c43ffc06f0115696e4296c81f70109fcde8d5296
Hyodar <franco.b.gomes@gmail.com>
2019-08-17 15:35:33 -0300

    Problema de Josephus

--- josephus/josephus.c
+++ josephus/josephus.c
@@ -1,5 +1,4 @@
 
-#include <string.h>
 #include "queue.h"
 
 int main(int argc, char** argv) {
@@ -19,7 +18,7 @@ int main(int argc, char** argv) {
     }
 
     while(!isQueueEmpty(queue)) {
-        for(int i=0; i<m-1; i++) {
+        for(int i = 0; i < m-1; i++) {
             int el = dequeue(queue);
             enqueue(queue, el);
         }
