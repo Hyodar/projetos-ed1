6d224dcc6bdead4a7edb949b3660771cb49c6ee4
Hyodar <franco.b.gomes@gmail.com>
2019-10-14 09:13:02 -0300

    Ordenação

---
+++ extra1/html.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+
+#include "../lista2/stack.h"
+
+int main() {
+
+	return 0;
+}
+
+int html() {
+
+	FILE* file = fopen("amostras/boatA.html", "r");
+	char line[128];
+
+	while(fgets
+}
